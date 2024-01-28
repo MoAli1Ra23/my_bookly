8e4df7a455de9ca35d7498ccbc578e697b65e98b
Muhammad Rashad <admoali123@gmail.com>
2024-01-28 22:21:08 +0200

    Create Custem list view item

--- lib/Features/home/persentation/widgets/home_view_body.dart
+++ lib/Features/home/persentation/widgets/home_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/Features/home/persentation/widgets/custom_list_view_item.dart';
+import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
- 
+
 import 'custom_appbar.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -11,6 +13,8 @@ class HomeViewBody extends StatelessWidget {
       child: Column(
         children: [
           CustomAppBar(),
+          CustomListViewItem()
+
         ],
       ),
     );
