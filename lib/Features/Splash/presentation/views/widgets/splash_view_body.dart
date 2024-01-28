c99407119e4078bebd80815fc8bdbfb7f97cca8c
Muhammad Rashad <admoali123@gmail.com>
2024-01-28 19:27:38 +0200

    my blookly

---
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,53 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashViewbody extends StatefulWidget {
+  const SplashViewbody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashViewbody> createState() => _SplashViewbodyState();
+}
+
+class _SplashViewbodyState extends State<SplashViewbody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> sliding;
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    sliding = Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+        .animate(CurvedAnimation(parent: _animationController, curve: Curves.bounceOut));
+    _animationController.forward();
+   
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(height: 4),
+        AnimatedBuilder(
+            animation: sliding,
+
+
+            builder: (context, _) {
+              
+              return SlideTransition(
+                position: sliding,
+                child:   Text(
+                  "Read Free Books",
+                  textAlign: TextAlign.center,
+                ),
+              );
+            }),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bookly/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'Features/Splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      home: const SplashView(),
+    );
+  }
+}
