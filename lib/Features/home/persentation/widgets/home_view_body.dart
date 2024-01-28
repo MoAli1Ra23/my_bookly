f9db87cbe3990459104a2870dd25b85aa2825ad3
Muhammad Rashad <admoali123@gmail.com>
2024-01-28 23:35:14 +0200

    best seller item  image

--- lib/Features/home/persentation/widgets/home_view_body.dart
+++ lib/Features/home/persentation/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/assets.dart';
 import '../../../../core/utils/styles.dart';
 import 'custom_appbar.dart';
 import 'featured_list_view.dart';
@@ -10,21 +11,50 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SafeArea(
-
       child: Padding(
-        padding: EdgeInsets.only(left:16.0),
+        padding: EdgeInsets.only(left: 16.0),
         child: Column(
-           crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CustomAppBar(),
             FeatureListView(),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Text(
-            "Best Seller",
-            style: Styles.titleMedium,
-            )
+              "Best Seller",
+              style: Styles.titleMedium,
+            ),
+            BestSallerListVeiwItem(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BestSallerListVeiwItem extends StatelessWidget {
+  const BestSallerListVeiwItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.test),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
