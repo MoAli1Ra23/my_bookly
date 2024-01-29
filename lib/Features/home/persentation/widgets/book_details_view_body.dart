6783e88ca53721ac66c0a680c8cfa9284ca24502
Muhammad Rashad <admoali123@gmail.com>
2024-01-29 12:22:38 +0200

    add titles and subtitles to it

--- lib/Features/home/persentation/widgets/book_details_view_body.dart
+++ lib/Features/home/persentation/widgets/book_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/styles.dart';
 import 'book_detials_custom_app_bar.dart';
 import 'custom_book_cover.dart';
 
@@ -9,18 +10,38 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var w= MediaQuery.of(context).size.width;
-    return   SafeArea(
+    var w = MediaQuery.of(context).size.width;
+    return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30.0),
         child: Column(
           children: [
             const BookDetailsCustomAppBar(),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: w*.17),
+              padding: EdgeInsets.symmetric(horizontal: w * .2),
               child: const CustomBookCover(),
             ),
-            
+            const SizedBox(
+              height: 43,
+            ),
+            Text(
+              'The Jungle Book',
+              style: Styles.textStyle30.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Opacity(
+              opacity: 0.7,
+              child: Text(
+                'Rudyard Kipling',
+                style: Styles.textStyle18.copyWith(
+                    fontWeight: FontWeight.w500, fontStyle: FontStyle.italic),
+              ),
+            ),
+
           ],
         ),
       ),
