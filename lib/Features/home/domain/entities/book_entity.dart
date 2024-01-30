a99e04ace37b82db1e45eb35326a66cf5927edbc
Muhammad Rashad <admoali123@gmail.com>
2024-01-30 09:57:33 +0200

    BookEntity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class BookEntity {
+  final String image; 
+  final String title; 
+  final String autherName; 
+  final num price; 
+  final num rateing; 
+  BookEntity({
+    required this.image,
+    required this.title,
+    required this.autherName,
+    required this.price,
+    required this.rateing,
+  });
+ 
+}
