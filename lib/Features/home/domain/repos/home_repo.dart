bef6702907988c6dbdb2edb73d225a383b0081c1
Muhammad Rashad <admoali123@gmail.com>
2024-01-30 10:45:49 +0200

    add either  and  failure

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,7 +1,9 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failure.dart';
 import '../entities/book_entity.dart';
 
-abstract class HomeRepo{
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewsetBooks();
-
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewsetBooks();
 }
