0d8ee884188973ebd1f5b62fa24b6409771ac52d
Muhammad Rashad <admoali123@gmail.com>
2024-01-30 17:42:49 +0200

    implemnt   remot fetchFeaturedbooks

--- lib/Features/home/data/data_source/home_remote_data_source.dart
+++ lib/Features/home/data/data_source/home_remote_data_source.dart
@@ -1,3 +1,6 @@
+import 'package:bookly/Features/home/data/models/book_moodel/book_moodel.dart';
+import 'package:bookly/core/utils/apiservice.dart';
+
 import '../../domain/entities/book_entity.dart';
 
 abstract class HomeRemotDateSource {
@@ -6,9 +9,23 @@ abstract class HomeRemotDateSource {
 }
 
 class HomeRepoImpl extends HomeRemotDateSource {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<List<BookEntity>> fetchFeaturedbooks() {
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedbooks() async {
+    var respons = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=programming');
+    List<BookEntity> books = formlistMapstoListBooks(respons);
+    return books;
+  }
+
+  List<BookEntity> formlistMapstoListBooks(Map<String, dynamic> respons) {
+    List<BookEntity> books = [];
+    for (var element in respons['items']) {
+      books.add(BookMoodel.fromMap(element));
+    }
+    return books;
   }
 
   @override
